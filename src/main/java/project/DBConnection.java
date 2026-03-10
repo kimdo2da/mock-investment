730bbc50fbf198ee990ab8ce412e5550eb26d29c
kimdo2da <107172886+kimdo2da@users.noreply.github.com>
2026-03-10 22:13:08 +0900

    Update DBConnection.java
    db 정보수정

--- src/main/java/project/DBConnection.java
+++ src/main/java/project/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 
     // 여기서 servletdb 로 변경
 	private static final String URL =
-		    "jdbc:mysql://localhost:3306/servletdb?serverTimezone=UTC&characterEncoding=UTF-8&useSSL=false&allowPublicKeyRetrieval=true";
+		    
 
 
     // MySQL 계정 정보 (네 PC에 맞게 바꿔줘)
